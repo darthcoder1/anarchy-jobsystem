7fd3cfe2249d8f8639c0ccf31e7fffb611b011d3
Martin Zielinski <martinz@unity3d.com>
2018-11-11 21:47:32 +0100

    Added tests for scheduler

--- src/bin/simple_test.rs
+++ src/bin/simple_test.rs
@@ -1,29 +1,32 @@
 #![feature(drain_filter)]
+#[allow(non_snake_case)]
 
 extern crate anarchy_jobsystem;
 
 use std::time::Duration;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicIsize,Ordering};
 use std::thread;
 
 use anarchy_jobsystem::scheduler;
 
 fn main() {
+
+    let mut sched = scheduler::initialize_scheduler(4);
+
+    let callId = Arc::new(AtomicIsize::new(0));
+
+    let job_start = Arc::new(scheduler::JobFence::new(true));
+
+    let jobA = sched.schedule_job(& [job_start], "Job A", scheduler::test_job_function_expected_call_id(callId.clone(), 0));
+    let jobB = sched.schedule_job(& [jobA.clone()], "Job B", scheduler::test_job_function_expected_call_id_range(callId.clone(), 1, 3));
+    let jobC = sched.schedule_job(& [jobA.clone()], "Job C", scheduler::test_job_function_expected_call_id_range(callId.clone(), 1, 3));
+    let jobD = sched.schedule_job(& [jobB.clone(), jobC.clone()], "Job D", scheduler::test_job_function_expected_call_id(callId.clone(), 3));
     
-    let mut job_sched = scheduler::initialize_scheduler(4);
+    assert!(jobD.wait(Duration::from_secs(1)));
+    assert!(jobD.is_signaled());
+    assert_eq!(4, callId.load(Ordering::Relaxed));
 
-    let fence = Arc::new(scheduler::JobFence::new(true));
-
-    let fence1 = job_sched.schedule_job(& [fence.clone()], Box::new(|| { println!("Execute JOB 0");}), "Job-A");
-    let fence2 = job_sched.schedule_job(& [fence1.clone()], Box::new(|| {println!("Execute JOB 1 after JOB 0" )}), "Job-B");
-    let fence3 = job_sched.schedule_job(& [fence2.clone()], Box::new(|| {println!("Execute JOB 2 after JOB 1" )}), "Job-C");
-    let fence4 = job_sched.schedule_job(& [fence.clone()], Box::new(|| {println!("Execute JOB 3" )}), "Job-C");
-
-    fence4.wait(Duration::from_millis(0));
-    thread::sleep(Duration::from_millis(1000));
-
-    scheduler::shutdown_scheduler(job_sched);
+    scheduler::shutdown_scheduler(sched);
     
-    println!("Hello World."); 
-    
-}
+} 
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,7 +1,7 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
-use std::sync::atomic::{AtomicBool,Ordering};
+use std::sync::atomic::{AtomicBool,AtomicIsize,Ordering};
 
 pub struct JobFence {
     signaled : AtomicBool,
@@ -12,10 +12,27 @@ impl JobFence {
         JobFence { signaled : AtomicBool::new(signaled) }
     }
 
-    pub fn wait(&self, timeout : Duration) {
-        while self.signaled.load(Ordering::Relaxed) {
-            thread::sleep(timeout);
+    pub fn is_signaled(&self) -> bool {
+        self.signaled.load(Ordering::Relaxed)
+    }
+
+    pub fn signal(&self) {
+        assert!(!self.is_signaled());
+        self.signaled.store(true, Ordering::Relaxed);
+    }
+
+    // Waits for the fence to become signaled or until the timeout.
+    // Returns either true (fence got signaled) or false (timeout)
+    pub fn wait(&self, timeout : Duration) -> bool{
+        let iter_timeout = Duration::from_millis(100);
+        let mut time_left = timeout;
+        
+        while !self.is_signaled() && time_left > Duration::new(0,0) {
+            thread::sleep(iter_timeout);
+            time_left -= iter_timeout;
         }
+
+        (time_left > Duration::new(0,0))
     }
 }
 
@@ -41,7 +58,7 @@ struct JobInfo {
 
 impl JobInfo {
     pub fn is_ready_to_run(&self) -> bool {
-        (self.dependencies.len() == 0 || self.dependencies.iter().any(|item| item.signaled.load(Ordering::Relaxed)))
+        (self.dependencies.len() == 0 || self.dependencies.iter().all(|item| item.signaled.load(Ordering::Relaxed)))
     }
 }
 
@@ -121,7 +138,12 @@ pub fn shutdown_scheduler(sched : JobScheduler) {
 
 impl JobScheduler {
 
-    pub fn schedule_job(& mut self, dependencies : & [Arc<JobFence>], job_func : Box<Fn() + Send + Sync + 'static>, job_name : &str) -> Arc<JobFence> {
+    pub fn schedule_job(& mut self, 
+                        dependencies : & [Arc<JobFence>], 
+                        job_name : &str, 
+                        job_func : Box<Fn() + Send + Sync + 'static>) -> Arc<JobFence> 
+    {
+        
         let job_info = JobInfo {
             name : String::from(job_name),
             dependencies: dependencies.to_vec(),
@@ -131,7 +153,7 @@ impl JobScheduler {
 
         let out_fence = job_info.out_fence.clone();
 
-        let mut was_empty = false;
+        let mut  was_empty = false;
         {
             let mut pending_jobs = self.controller.pending_jobs.lock().unwrap();
             was_empty = pending_jobs.len() == 0;
@@ -148,24 +170,152 @@ impl JobScheduler {
 
 fn scheduler_thread_function(controller : & JobController, thread_idx : i32) {
 
-    println!("Spawn Thread {}", thread_idx);
-
     loop {
-
         let next_action = controller.get_next_action();
 
         match next_action {
             JobControllerStatus::ExecuteJob(job) => {
-                println!("Execute job {}", job.name);
                 (job.func)();
-                job.out_fence.signaled.store(true, Ordering::Relaxed);
-
+                job.out_fence.signal();
                 controller.update_ready_to_run_jobs();
             },
-            JobControllerStatus::WaitForJob => thread::sleep(Duration::from_millis(0)),
+            JobControllerStatus::WaitForJob => {
+                thread::sleep(Duration::from_millis(0));
+                // TODO: fall asleep until jobs are in queue
+                controller.update_ready_to_run_jobs();
+            },
             JobControllerStatus::ExitRequested => break,
         }
     }
+}
 
-    println!("Kill Thread {}", thread_idx);
+// returns a function for a job, that increments the atomic counter after it checked whether the current value of the counter
+// is equal to expected_call_idx
+pub fn test_job_function_expected_call_id(callId_counter : Arc<AtomicIsize>, expected_call_idx : isize) -> Box<Fn() + Send + Sync + 'static>
+{
+    let call_id = callId_counter.clone();
+
+    Box::new (move || { 
+        let id = call_id.load(Ordering::Relaxed);
+        call_id.fetch_add(1, Ordering::Relaxed);
+
+        assert_eq!(expected_call_idx, id);
+    })
+}
+
+// returns a function for a job, that increments the atomic counter after it checked whether the current value of the counter
+// is within the range specified with min_call_idx(inclusive) and max_call_idx(exklusiv)
+pub fn test_job_function_expected_call_id_range(callId_counter : Arc<AtomicIsize>, min_call_idx : isize, max_call_idx : isize) 
+    -> Box<Fn() + Send + Sync + 'static>
+{
+    Box::new (move || { 
+        let id = callId_counter.load(Ordering::Relaxed);
+        
+        callId_counter.fetch_add(1, Ordering::Relaxed);
+
+        assert!(id >= min_call_idx && id <  max_call_idx);
+    })
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::super::scheduler;
+
+    use std::time::Duration;
+    use std::sync::atomic::{AtomicIsize,Ordering};
+    use std::sync::Arc;
+
+    use scheduler::test_job_function_expected_call_id;
+    use scheduler::test_job_function_expected_call_id_range;
+
+    #[test]
+    fn JobFence_wait_ReturnsTrueAfterSignaled() {
+        let mut fence = scheduler::JobFence::new(false);
+        
+        assert!(!fence.is_signaled());
+        
+        fence.signal();
+        
+        assert!(fence.is_signaled());
+        assert!(fence.wait(Duration::from_millis(100)));
+    }
+
+    #[test]
+    fn JobFence_wait_ReturnsFalseAfterTimeout() {
+        let mut fence = scheduler::JobFence::new(false);
+        
+        assert!(!fence.is_signaled());
+        assert!(!fence.wait(Duration::from_millis(100)));
+    }
+
+    #[test]
+    fn JobScheduler_With4Threads_InitializesThreadsCorrectly(){
+    
+        let mut sched = scheduler::initialize_scheduler(4);
+
+        assert_eq!(4, sched.threads.len());
+        scheduler::shutdown_scheduler(sched);
+    }
+
+    #[test]
+    fn JobScheduler_Executes1Job(){
+
+        let mut sched = scheduler::initialize_scheduler(4);
+
+        let callId = Arc::new(AtomicIsize::new(0));
+
+        let fence = sched.schedule_job(& [Arc::new(scheduler::JobFence::new(true))], "TestJob 0", test_job_function_expected_call_id(callId.clone(), 0));
+
+        assert!(fence.wait(Duration::from_secs(1)));
+        assert!(fence.is_signaled());
+        assert_eq!(1, callId.load(Ordering::Relaxed));
+
+        scheduler::shutdown_scheduler(sched);
+    }
+
+    #[test]
+    fn JobScheduler_ExecutesJob1AfterJob0(){
+
+        let mut sched = scheduler::initialize_scheduler(4);
+
+        let callId = Arc::new(AtomicIsize::new(0));
+
+        let job_start = Arc::new(scheduler::JobFence::new(true));
+        let job0 = sched.schedule_job(& [job_start], "Job 0", test_job_function_expected_call_id(callId.clone(), 0));
+        let job1 = sched.schedule_job(& [job0.clone()], "Job 1", test_job_function_expected_call_id(callId.clone(), 1));
+
+        assert!(job1.wait(Duration::from_secs(1)));
+        assert!(job1.is_signaled());
+        assert!(job0.is_signaled());
+        assert_eq!(2, callId.load(Ordering::Relaxed));
+
+        scheduler::shutdown_scheduler(sched);
+    }
+
+    #[test]
+    fn JobScheduler_ExecutesChain_A_BC_D(){
+
+        let mut sched = scheduler::initialize_scheduler(4);
+
+        let callId = Arc::new(AtomicIsize::new(0));
+
+        let job_start = Arc::new(scheduler::JobFence::new(true));
+
+        let jobA = sched.schedule_job(& [job_start], "Job A", scheduler::test_job_function_expected_call_id(callId.clone(), 0));
+        let jobB = sched.schedule_job(& [jobA.clone()], "Job B", scheduler::test_job_function_expected_call_id_range(callId.clone(), 1, 3));
+        let jobC = sched.schedule_job(& [jobA.clone()], "Job C", scheduler::test_job_function_expected_call_id_range(callId.clone(), 1, 3));
+        let jobD = sched.schedule_job(& [jobB.clone(), jobC.clone()], "Job D", scheduler::test_job_function_expected_call_id(callId.clone(), 3));
+        
+        assert!(jobD.wait(Duration::from_secs(1)));
+        assert!(jobD.is_signaled());
+        assert!(jobA.is_signaled());
+        assert!(jobB.is_signaled());
+        assert!(jobC.is_signaled());
+        
+        assert_eq!(4, callId.load(Ordering::Relaxed));
+
+        scheduler::shutdown_scheduler(sched);
+    }
 }
