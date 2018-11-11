1b353baca70d18740cee9c1f9842047abc17aed9
Martin Zielinski <darthcoder1@gmail.com>
2018-11-11 15:19:15 +0100

    Proper usage of rust Mutex

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -8,42 +8,49 @@ pub struct JobFence {
 }
 
 pub struct JobScheduler{
+    // JoinHandles for all running job threads
     threads : Vec<thread::JoinHandle<()>>,
-    pending_jobs : Vec<JobInfo>,
+    // Vector of jobs that still need to be executed
+    pending_jobs : Mutex<Vec<JobInfo>>,
+    // dispatcher object to allow job threads to get access to the
+    // pending_jobs
     dispatcher : Arc<JobDispatcher>
 }
 
 struct JobInfo {
+    // Vector of dependent job fences, aka this Job can
+    // not run before all dependecies have run
     dependencies : Vec<Arc<JobFence>>,
-    func : Box<Fn()>,
+    // job function to execute
+    func : Box<Fn() + Send + Sync + 'static>,
+    // fence to trigger when the job is finished
     out_fence : Arc<JobFence>,
 }
 
-
 struct JobDispatcher {
-    ready_to_run : Vec<JobInfo>,
-    lock : Mutex<i32>
+    // Vector of ready to run jobs (jobs with all dependencies sigaled)
+    ready_to_run : Mutex<Vec<JobInfo>>,
 }
 
 impl JobDispatcher {
 
     fn new() -> JobDispatcher {
         JobDispatcher {
-            ready_to_run: vec![],
-            lock: Mutex::new(0),
+            ready_to_run: Mutex::new(vec![]),
         }
     }
 
-    fn push_job_to_run(& mut self, job : JobInfo) {
-        let _guard = self.lock();
-        self.ready_to_run.push(job);
+    fn push_job_to_run(&self, job : JobInfo) {
+        let mut job_list = self.ready_to_run.lock().unwrap();
+
+        job_list.push(job);
     }
 
-    fn pull_job_to_run(& mut self) -> Result<JobInfo, ()> {
-        let _guard = self.lock();
+    fn pull_job_to_run(&self) -> Result<JobInfo, ()> {
+        let mut job_list = self.ready_to_run.lock().unwrap();
         
-        if self.ready_to_run.len() > 0 {
-            Ok(self.ready_to_run.pop().unwrap())
+        if job_list.len() > 0 {
+            Ok(job_list.pop().unwrap())
         } else {
             Err(())
         }
@@ -54,13 +61,13 @@ pub fn initialize_scheduler(num_job_threads : i32) -> JobScheduler {
 
     let mut sched = JobScheduler {
         threads : vec![],
-        pending_jobs : vec![],
+        pending_jobs : Mutex::new(vec![]),
         dispatcher: Arc::new(JobDispatcher::new()),
     };
 
     for i in 0..num_job_threads {
-        let dispatcher = sched.dispatcher.clone();
-        sched.threads.push(thread::spawn(move || { scheduler_thread_function(dispatcher, i); } ));
+        let mut dispatcher = sched.dispatcher.clone();
+        sched.threads.push(thread::spawn(move || { scheduler_thread_function(& dispatcher, i); } ));
     }
 
     sched
@@ -75,7 +82,7 @@ pub fn shutdown_scheduler(sched : JobScheduler) {
 
 impl JobScheduler {
 
-    pub fn schedule_job(& mut self, dependencies : & [Arc<JobFence>], job_func : Box<Fn()>) -> Arc<JobFence> {
+    pub fn schedule_job(& mut self, dependencies : & [Arc<JobFence>], job_func : Box<Fn() + Send + Sync + 'static>) -> Arc<JobFence> {
         let job_info = JobInfo {
             dependencies: dependencies.to_vec(),
             func: job_func,
@@ -83,17 +90,25 @@ impl JobScheduler {
         };
 
         let out_fence = job_info.out_fence.clone();
-        self.pending_jobs.push(job_info);
+
+        self.pending_jobs.lock().unwrap()
+            .push(job_info);
         
         out_fence
     }
 }
 
+fn scheduler_thread_function(dispatcher : & JobDispatcher, thread_idx : i32) {
 
-
-fn scheduler_thread_function(dispatcher : Arc<JobDispatcher>, thread_idx : i32) {
     println!("Spawn Thread {}", thread_idx);
 
-    
-    println!("Kill Thread {}", thread_idx);
+    loop {
+
+        let job_to_run = dispatcher.pull_job_to_run();
+
+        match job_to_run {
+            Ok(job) => (job.func)(),
+            _ => (),
+        }
+    }
 }
